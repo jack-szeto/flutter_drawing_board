dcec8c7b98df90b5c8505f324caccd94fe180320
jack-szeto <fhy26548726@gmail.com>
2025-08-26 00:27:18 +0800

    feat: export Pointer tool in paint_contents.dart

--- lib/paint_contents.dart
+++ lib/paint_contents.dart
@@ -2,6 +2,7 @@ export 'src/paint_contents/circle.dart';
 export 'src/paint_contents/empty_content.dart';
 export 'src/paint_contents/eraser.dart';
 export 'src/paint_contents/paint_content.dart';
+export 'src/paint_contents/pointer.dart';
 export 'src/paint_contents/rectangle.dart';
 export 'src/paint_contents/simple_line.dart';
 export 'src/paint_contents/smooth_line.dart';
